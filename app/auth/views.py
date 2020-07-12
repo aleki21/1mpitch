6dddebbf23e0c226a1621e4404e5f64f5b60b038
alex <alexkimutai021@gmail.com>
2020-07-12 21:46:16 +0300

    Creating profile page

--- app/auth/views.py
+++ app/auth/views.py
@@ -37,3 +37,21 @@ def register():
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
 
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,17 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
 from flask_login import login_required
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/home/comments/new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_review(id):
